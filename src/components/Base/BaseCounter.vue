c1ada1f16dadc496ebc68eb180b9de675aca9d3c
Jawad Ali <alijamil734@gmail.com>
2021-08-27 17:22:48 +0500

    made base counter

---
+++ src/components/Base/BaseCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="inline-block">
+    <div class="flex flex-col text-sm items-center">
+      <p class="bg-gray-50 border border-gray-300 w-10 h-10 flex justify-center items-center rounded-full">
+
+      <slot name="counter"></slot>
+      </p>
+      <p class="mt-2"><slot></slot></p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
