5c7a075218b48db18512690a54e947dd2793d230
Jawad Ali <alijamil734@gmail.com>
2021-08-27 17:06:44 +0500

    Made base avatar component

---
+++ src/components/Base/BaseAvatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="
+      bg-blue-500
+      w-7
+      h-7
+      flex
+      justify-center
+      items-center
+      text-white
+      rounded-full
+    "
+  >
+    <p class="text-lg">{{title}}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: String
+  })
+</script>
