491900ecb6f158b1553df3aa1503f522826d32b6
Jawad Ali <alijamil734@gmail.com>
2021-08-27 17:07:04 +0500

    made base category flag's component

---
+++ src/components/Base/BaseCategoryFlag.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="
+      text-xs
+      bg-gray-50
+      border border-gray-300
+      px-2
+      py-1
+      rounded-full
+      inline-block
+      cursor-pointer
+      hover:bg-gray-100
+      active:bg-gray-200
+    "
+  >
+    <slot></slot>
+  </div>
+</template>
