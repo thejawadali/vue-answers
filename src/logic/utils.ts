559eeeddec07c4ad853fbdf38d252a22c4f36f73
Jawad Ali <alijamil734@gmail.com>
2021-09-04 14:48:03 +0500

    make toaster

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -1,6 +1,9 @@
 import dayjs from "dayjs"
 import duration from "dayjs/plugin/duration"
 import relativeTime from "dayjs/plugin/relativeTime"
+import { createToast, ToastType } from "mosha-vue-toastify"
+import "mosha-vue-toastify/dist/style.css"
+
 dayjs.extend(duration)
 dayjs.extend(relativeTime)
 
@@ -14,4 +17,19 @@ export function getFirstLetterOfName(name: string) {
 
 export function timeDifference(posted: Date) {
   return dayjs.duration(dayjs(posted).diff(dayjs())).humanize(true)
+}
+
+
+export const toast = (description: string, type: ToastType) => {
+  createToast(
+    { description },
+    {
+      position: "bottom-right",
+      showIcon: true,
+      timeout: 3000,
+      hideProgressBar: true,
+      transition: "slide",
+      type
+    }
+  )
 }
